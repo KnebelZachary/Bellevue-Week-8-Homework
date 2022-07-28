385e5d13841a07ec42b08ddb94ff9a376b234278
KnebelZachary <110074104+KnebelZachary@users.noreply.github.com>
2022-07-27 20:52:55 -0400

    created sample files

---
+++ sample.py
@@ -0,0 +1,17 @@
+#sample file to have in repo for assignement
+
+from time import sleep
+
+
+print("This is a sample Python project")
+
+question1 = "Type a message and I will repeat it back to you! (Type 'quit' to end the program):"
+
+while True:
+    message = input (question1)
+    if  message == "quit":
+        print("Thanks for playing!")
+        break
+    else:
+        print(message)
+        sleep(2)
